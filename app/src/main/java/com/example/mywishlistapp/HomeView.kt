f0631039f11fdb1a9c902fd2448f79ad3e31d2fc
m4DARA <king_mola@yahoo.com>
2024-10-27 23:24:33 +0300

    MyWishListApp

---
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -0,0 +1,11 @@
+package com.example.mywishlistapp
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeView(){
+    Scaffold {
+
+    }
+}
