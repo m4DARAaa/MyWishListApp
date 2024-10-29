61993fe3563f38b76d5e55cbef8774b8d91aca92
m4DARA <king_mola@yahoo.com>
2024-10-29 21:04:50 +0300

    MyWishListApp

--- app/src/main/java/com/example/mywishlistapp/AppBar.kt
+++ app/src/main/java/com/example/mywishlistapp/AppBar.kt
@@ -2,20 +2,13 @@ package com.example.mywishlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material.TopAppBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBarView(
     title: String,
@@ -31,13 +24,7 @@ fun AppBarView(
                     .heightIn(max = 24.dp)
             )
         },
-        navigationIcon = {
-            IconButton(onClick = { onBackNavClicked() }) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
-            }
-        },
-        colors = TopAppBarDefaults.smallTopAppBarColors(
-            containerColor = colorResource(id = R.color.app_bar_color)
-        )
+        elevation = 3.dp,
+        backgroundColor = colorResource(id = R.color.app_bar_color)
     )
 }
