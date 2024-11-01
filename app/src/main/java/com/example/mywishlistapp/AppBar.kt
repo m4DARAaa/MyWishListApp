e513ac3b71402ebba8832ae205804c57aa4055c8
m4DARA <king_mola@yahoo.com>
2024-11-01 08:06:54 +0300

    MyWishListApp

--- app/src/main/java/com/example/mywishlistapp/AppBar.kt
+++ app/src/main/java/com/example/mywishlistapp/AppBar.kt
@@ -2,10 +2,15 @@ package com.example.mywishlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -14,6 +19,21 @@ fun AppBarView(
     title: String,
     onBackNavClicked: () -> Unit = {}
 ) {
+    val navigationIcon: (@Composable () -> Unit)? =
+        if (!title.contains("WishList")) {
+            {
+                IconButton(onClick = { onBackNavClicked() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        tint = Color.White,
+                        contentDescription = null
+                    )
+                }
+            }
+        } else {
+            null
+        }
+
     TopAppBar(
         title = {
             Text(
@@ -25,6 +45,7 @@ fun AppBarView(
             )
         },
         elevation = 3.dp,
-        backgroundColor = colorResource(id = R.color.app_bar_color)
+        backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
     )
 }
--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,20 +1,45 @@
 package com.example.mywishlistapp
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
+    val context = LocalContext.current
     Scaffold(
-        topBar = { AppBarView(title = "WishList") }
+        topBar = {
+            AppBarView(title = "WishList") {
+                Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+            }
+        },
+        floatingActionButton ={
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                containerColor=Color.Black,
+                onClick = {  Toast.makeText(context, "Float Button Clicked", Toast.LENGTH_LONG).show() }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription =null )
+            }
+        }
+        
     ) {
-        LazyColumn(modifier = Modifier
-            .fillMaxSize()
-            .padding(it)) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
 
         }
     }
