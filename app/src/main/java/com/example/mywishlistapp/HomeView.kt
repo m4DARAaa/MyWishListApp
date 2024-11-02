4f51666f1e264aa2d7e26a549dabb0052e7a22f1
m4DARA <king_mola@yahoo.com>
2024-11-02 23:53:20 +0300

    MyWishListApp

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,18 +1,25 @@
 package com.example.mywishlistapp
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.mywishlistapp.data.Wish
 
@@ -25,16 +32,18 @@ fun HomeView() {
                 Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
             }
         },
-        floatingActionButton ={
+        floatingActionButton = {
             FloatingActionButton(
                 modifier = Modifier.padding(all = 20.dp),
                 contentColor = Color.White,
-                containerColor=Color.Black,
-                onClick = {  Toast.makeText(context, "Float Button Clicked", Toast.LENGTH_LONG).show() }) {
-                Icon(imageVector = Icons.Default.Add, contentDescription =null )
+                containerColor = Color.Black,
+                onClick = {
+                    Toast.makeText(context, "Float Button Clicked", Toast.LENGTH_LONG).show()
+                }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
         }
-        
+
     ) {
         LazyColumn(
             modifier = Modifier
@@ -45,5 +54,21 @@ fun HomeView() {
         }
     }
 }
+
 @Composable
-fun WishItem(wish: Wish, onClick:()->Unit){}
+fun WishItem(wish: Wish, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+            .clickable { onClick() },
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+        border = BorderStroke(1.dp, Color.White)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)
+        }
+
+    }
+}
