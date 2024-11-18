966e4c44c68902d445aa3b6e01887199884cd953
m4DARA <king_mola@yahoo.com>
2024-11-18 23:02:53 +0300

    MyWishListApp

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
@@ -21,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.mywishlistapp.data.DummyWish
 import com.example.mywishlistapp.data.Wish
 
 @Composable
@@ -49,7 +51,12 @@ fun HomeView() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
-        ) {
+        ) {items(DummyWish.wishList){
+            wish->
+            WishItem(wish = wish) {
+                
+            }
+        }
 
         }
     }
--- app/src/main/java/com/example/mywishlistapp/data/Wish.kt
+++ app/src/main/java/com/example/mywishlistapp/data/Wish.kt
@@ -3,3 +3,15 @@ package com.example.mywishlistapp.data
 data class Wish(val id: Long = 0L,
                 val title: String = "",
                 val description: String = "")
+object DummyWish{
+    val wishList= listOf(
+        Wish(title = "monitor", description = "2k/ 240Hz"),
+        Wish(title = "vga", description = "rx6800xt"),
+        Wish(title = "cpu", description = "ryzen 7 9800x3d")
+
+
+
+
+
+    )
+}
