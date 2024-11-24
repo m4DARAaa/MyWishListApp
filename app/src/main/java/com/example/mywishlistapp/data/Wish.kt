0eebb48dbd5ac68f4eefd04a8d3e1b83626133ba
m4DARA <king_mola@yahoo.com>
2024-11-24 23:31:13 +0300

    MyWishListApp

--- app/src/main/java/com/example/mywishlistapp/data/Wish.kt
+++ app/src/main/java/com/example/mywishlistapp/data/Wish.kt
@@ -1,16 +1,24 @@
 package com.example.mywishlistapp.data
 
-data class Wish(val id: Long = 0L,
-                val title: String = "",
-                val description: String = "")
-object DummyWish{
-    val wishList= listOf(
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wish_table")
+data class Wish(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    @ColumnInfo("wish_title")
+    val title: String = "",
+    @ColumnInfo("wish_desc")
+    val description: String = ""
+)
+
+object DummyWish {
+    val wishList = listOf(
         Wish(title = "monitor", description = "2k/ 240Hz"),
         Wish(title = "vga", description = "rx6800xt"),
         Wish(title = "cpu", description = "ryzen 7 9800x3d")
-
-
-
 
 
     )
