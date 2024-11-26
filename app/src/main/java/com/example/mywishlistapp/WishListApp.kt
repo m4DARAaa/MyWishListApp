aa14274d386d4a78d65a555109795b0124d1248d
m4DARA <king_mola@yahoo.com>
2024-11-26 23:22:46 +0300

    MyWishListApp

---
+++ app/src/main/java/com/example/mywishlistapp/WishListApp.kt
@@ -0,0 +1,16 @@
+package com.example.mywishlistapp
+
+import android.app.Application
+
+class WishListApp:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Graph.provide(this)
+    }
+
+
+
+
+
+
+}
